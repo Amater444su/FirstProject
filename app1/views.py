1747c73391807436c13ae5324ba5c0175c0b4514
Amater444su <323.5432@gmail.com>
2021-07-26 16:34:25 +0300

    Removed comments

--- app1/views.py
+++ app1/views.py
@@ -1,14 +1,12 @@
 import ipdb
 from django.conf import settings
-from django.contrib import messages
+
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.mail import send_mail
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView, View
 from django.views.generic.edit import FormMixin
-# FormMixin с класа генерик, добавляет возможность подключать форму,
-# подробнее: (https://github.com/django/django/blob/main/django/views/generic/edit.py)
 from .models import Product, ProductType, Cart, Comment, Profile, WishList, Message, EmailMessage
 from .forms import ProductForm, CommentForm, UserForm, MessageForm
 from django.urls import reverse, reverse_lazy
@@ -18,9 +16,8 @@ from .utils import count_total_product_amount
 
 class ProductListView(ListView):
     """Выводит список всех товаров"""
-    # model = Product     # Моя модель ( таблица с БД )
-    template_name = 'home.html'  # Сылка как в функции
-    context_object_name = 'list_products'  # Контекстк который раньше передавал в функции
+    template_name = 'home.html'
+    context_object_name = 'list_products'
 
     def get_context_data(self, *args, **kwargs):
         """Передает дополнительный контекст
@@ -46,21 +43,6 @@ class ProductListView(ListView):
         else:
             queryset = Product.objects.all()
         return queryset
-
-
-# def home(request):
-#
-#     if request.query_params:
-#         product_type = request.query_params.get('type')
-#         list_product = Product.objects.get(type=product_type)
-#     else:
-#         list_product = Product.objects.all()
-#
-#     context = {
-#         'list_product': list_product
-#
-#     }
-#     return render(request, 'home.html', context)
 
 
 class HomeDetailView(DetailView, FormMixin, SuccessMessageMixin):
@@ -89,16 +71,6 @@ class HomeDetailView(DetailView, FormMixin, SuccessMessageMixin):
 
 
 
-# def detail(request, id):
-
-#     """Вывод товара с БД"""
-#     get_product = Product.objects.get(id=id)
-#     context = {
-#         'get_product': get_product
-#     }
-#     return render(request, 'detail.html', context)
-
-
 class ProductCreateView(LoginRequiredMixin, CreateView):
     """Создание товара в БД"""
     model = Product
@@ -118,25 +90,6 @@ class ProductCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
-# def edit_page(request):
-#
-#     suc = False
-#
-#     if request.method == 'POST':    # Если в моей html странице form method имеет значние пост
-#         form = Productform(request.POST)    # (request.POST) получаем параметры с браузера
-#         if form.is_valid():     # Если форма валидна, тоесть все прохидит то идем дальше :)
-#             form.save()     # Сохраняем значения юзера
-#             suc = True
-#
-#     context = {
-#         'list_product': Product.objects.all().order_by('id'),
-#         'form': Productform(),
-#         'suc': suc
-#
-#     }
-#     return render(request, 'edit_page.html', context)
-
-
 class ProductUpdateView(LoginRequiredMixin, UpdateView):
     """Обновление товара в БД"""
     model = Product
@@ -148,29 +101,6 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
         kwargs['update'] = True
         return super().get_context_data(**kwargs)
 
-    # def get_form_kwargs(self):
-    #     kwargs = super().get_form_kwargs()
-    #     if self.request.user != kwargs['instance'].author:
-    #         return self.handle_no_permission()
-    #     return kwargs
-
-
-# def update_page(request, id):
-#
-#     if request.method == 'POST':    # Если в моей html странице form method имеет значние пост
-#         form = Productform(request.POST, instance=Product.objects.get(id=id))    # (request.POST) получаем параметры
-#         # с браузера
-#         if form.is_valid():     # Если форма валидна, тоесть все проходит то идем дальше :)
-#             form.save()     # Сохраняем значения юзера
-#
-#     context = {
-#
-#         'get_product': Product.objects.get(id=id),
-#         'form': Productform(instance=Product.objects.get(id=id)),
-#         'update': True
-#     }
-#     return render(request, 'edit_page.html', context)
-
 
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     """Удаление товара"""
@@ -178,23 +108,10 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
     template_name = 'edit_page.html'
     success_url = reverse_lazy('edit_page')
 
-    # Для работы этого класса необходимо написать форму
-    # <form action="{% url 'delete_page' i.id %}" method="post">{% csrf_token %}
-
-    # def delete(self, request, *args, **kwargs):
-    #     success_url = reverse_lazy('profile_products')
-    #     self.object = self.get_object()
-    #     if self.request.user != self.object.author:
-    #         return self.handle_no_permission()
-    #     self.object.delete()
-    #     return HttpResponseRedirect(success_url)
-    #     # return HttpResponse()
-
 
 class CommentDeleteView(LoginRequiredMixin, DeleteView):
     """Удаление коментария"""
     model = Comment
-    # success_url = '/detail/'
 
     def get_success_url(self):
         # ipdb.set_trace()
@@ -210,16 +127,6 @@ class CommentDeleteView(LoginRequiredMixin, DeleteView):
             return self.handle_no_permission()
         self.object.delete()
         return HttpResponseRedirect(success_url)
-        # return HttpResponse()
-
-    # def delete(self, request, *args, **kwargs):
-    #     success_url = reverse_lazy('profile_products')
-    #     self.object = self.get_object()
-    #     if self.request.user != self.object.author:
-    #         return self.handle_no_permission()
-    #     self.object.delete()
-    #     return HttpResponseRedirect(success_url)
-    #     # return HttpResponse()
 
 
 class ProfileView(ListView):
@@ -284,15 +191,12 @@ class CartView(LoginRequiredMixin, View, SuccessMessageMixin):
 
     def post(self, request, product_id):
        # ipdb.set_trace()
-        user = request.user  # Достали юзера и записали юзера, который сейчас залогинен нас сайт
+        user = request.user
         product = Product.objects.filter(
-            id=product_id).first()  # Достал определенный товар(продукт), с формы получил пк
-        # при помощи пк и достал его, так же метод 'first()' возвращает первый елемен
-        # user_cart = Cart.objects.filter(owner=user)
-        user_cart, created = Cart.objects.get_or_create(  # создаем или берем уже готовую инстанц модель
-            owner=user,  # берем если уже создано
-            defaults={  # в дефолт записываем, если инстанц модели небыл создан
-                # 'products': product,
+            id=product_id).first()
+        user_cart, created = Cart.objects.get_or_create(
+            owner=user,
+            defaults={
                 'total_products': 1,
                 'final_price': product.price
             }
@@ -300,13 +204,6 @@ class CartView(LoginRequiredMixin, View, SuccessMessageMixin):
 
         user_cart.products.add(product)
         return HttpResponseRedirect('/')
-        # return render(request, 'cart.html', context)
-
-    def get_success_message(self, cleaned_data):
-        print(cleaned_data)
-        return "Товар успешно добавлен в корзину"
-    # get = Выводит данные, не меняя данных!
-    # post = Прийнмает новые данные, и созднает новый обьект!
 
 
 class ProfileUpdateView(UpdateView):
@@ -333,7 +230,6 @@ class ProductDeleteFromCartView(View):
 def get_email(request, detail_id):
     """Отправка емейла владельцу товара"""
     # ipdb.set_trace()
-    # form = MessageForm()
     action = request.POST['action']
     message = 'qwe'
     if request.method == 'POST' and action == 'make_message':
