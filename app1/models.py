a90d046ad1bce795941baa2cc1a749351d82abf1
Amater444su <323.5432@gmail.com>
2021-07-26 20:48:37 +0300

    removed text

--- app1/models.py
+++ app1/models.py
@@ -15,8 +15,8 @@ class Profile(AbstractUser):
 
 
 class ProductType(models.TextChoices):
-    SNEAKERS = 'Кросовки'  # Создаю класс 'ProductType', класс выбора, в нем я обьявляю свои переменные (Теги
-    GUMSHOES = 'Кеды'  # по которым я буду сортировать/фильтровать), название с большой буквы
+    SNEAKERS = 'Кросовки'
+    GUMSHOES = 'Кеды'
     CLOGS = 'Сабо'
     CROCS = 'Кроги'
     EARTH_SHOES = 'Легкие кросовки'
@@ -25,26 +25,19 @@ class ProductType(models.TextChoices):
     SLIPPERS = 'Тапки'
     OXFORDS = 'Оксофорды'
     HIGH_HEELS = 'Туфли на каблуке'
-    # этот класс нужно передать в CharField моей основной модели ( в моем случае 'Product' )
 
 
 class Product(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE, verbose_name='Пользователь')
     create_data = models.DateTimeField(auto_now_add=True,
-                                       verbose_name='Дата создания')  # Время в живую поскольку параметр Тру
-    name = models.CharField(max_length=30, verbose_name='Имя')  # Максимальна длина строки
-    text = models.TextField()  # Тест ( много текста ) в отличии от 'CharField' Который принимает определенное
-    # кол-то текста, 'TextField' принимет инфинити текста.
+                                       verbose_name='Дата создания')
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    text = models.TextField()
     price = models.IntegerField(null=True, verbose_name='Цена')
     image = models.ImageField(default='images/no.png', upload_to='images/', null=True, blank=True,
                               verbose_name='Картинка')
     type = models.CharField(choices=ProductType.choices, max_length=40)  # 'choices' передаю свой класс 'ProductType'
 
-    # null=True означет что в базе данных может
-    # быть пустое место, может и не быть картинки, blank=True в запросте на бэкенд может не приходить.
-    # type = models.ForeignKey('ProductType', null=True, on_delete=models.PROTECT, verbose_name='Категория')
-    # PROTECT = если я удалю тип(связь) то моя модель ( Кверисет ) останеться
-    # CASCADE = если я удалю свою свзять (тип) то модель ( кверисет ) тоже удалиться
 
     def __str__(self):
         return self.name
