1b975745053b113e8a7ebdcc19335620fcfb4bd3
PavloGrytsenko <grytsenkopavlo20@gmail.com>
2021-12-09 17:37:30 +0200

    Refactored, fixed errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py makemigrations --no-input
 
 python manage.py migrate --no-input
 
-python manage.py runserver 0.0.0.0:8080
+exec gunicorn somesite.wsgi:application -b 0.0.0.0:8000 --reload
